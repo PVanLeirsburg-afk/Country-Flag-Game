697721ca070a72935a0b50d2e6d499059d3f89d5
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-08 16:00:26 -0500

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Pamela VanLeirsburg on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

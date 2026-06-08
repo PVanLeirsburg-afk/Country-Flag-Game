d77bb193b51a72215c314544e5d0bf4606f627ca
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-08 17:09:01 -0500

    Added QuestionView

---
+++ Country Flag Game/Views/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  Country Flag Game
+//
+//  Created by Pamela VanLeirsburg on 6/8/26.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionView()
+}

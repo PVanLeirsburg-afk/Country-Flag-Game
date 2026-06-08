99917e4762928629b958791257bad469c26ea668
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-08 16:46:29 -0500

    Created Progress Bar

---
+++ Country Flag Game/Components/ProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBar.swift
+//  Country Flag Game
+//
+//  Created by Pamela VanLeirsburg on 6/8/26.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
